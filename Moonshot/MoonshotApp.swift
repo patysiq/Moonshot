9ff2984f3a88b32347a4e8d92065f4452a8f4d1f
Patricia <patysiq@yahoo.com.br>
2021-01-27 11:27:34 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by PATRICIA S SIQUEIRA on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
