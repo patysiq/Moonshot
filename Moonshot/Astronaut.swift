aa3f85c05de423968ead3af161517e80b039e9df
Patricia <patysiq@yahoo.com.br>
2021-01-28 16:12:36 -0300

    add app

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by PATRICIA S SIQUEIRA on 27/01/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
